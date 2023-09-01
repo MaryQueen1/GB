15cef21ac032de4b3a2ab818af5eeb332a1b33e5
Mary <maryqueenk1@gmail.com>
2023-09-01 20:51:55 +0300

    seminar about java number one

---
+++ java. introduction and how to use the basic api/l1. getting to know the java programming language/program.java
@@ -0,0 +1,9 @@
+// сниппет cl
+public class program {
+    // точка входа ma
+    public static void main(String[] args) {
+        // связь с консолью sout
+        System.out.println("Hello world");
+    }
+}
+
