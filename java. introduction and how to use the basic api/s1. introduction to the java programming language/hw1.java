7b13139b9a80f02f69e1c58d218570b2dd3fb665
Mary <maryqueenk1@gmail.com>
2023-09-04 14:50:18 +0300

    ad the answers to hw1 and hw2

--- java. introduction and how to use the basic api/s1. introduction to the java programming language/hw1.java
+++ java. introduction and how to use the basic api/s1. introduction to the java programming language/hw1.java
@@ -1,14 +1,23 @@
 import java.util.Random;
 
 public class hw1 {
-    public int countNTriangle(int n) {
+
+    // public int countNTriangle(int n){
+    //     int sum = 0;
+    //     for(int i = 1; i <= n; i++){
+    //         sum += i;
+    //     }
+    //     return sum;
+    // }
+    
+    public static int countNTriangle(int n) {
         return (n * (n + 1)) / 2;
     }
 
-    public void main (String[] args) {
+    public static void main (String[] args) {
         Random random = new Random();
         int n = random.nextInt(101);
         int nthTriangularNumber = countNTriangle(n);
-        System.out.println("The \" + n + \"-th triangular number is: " + nthTriangularNumber);
+        System.out.println("The " + n + "-th triangular number is: " + nthTriangularNumber);
     }
 }
---
+++ java. introduction and how to use the basic api/s1. introduction to the java programming language/hw2.java
@@ -0,0 +1,38 @@
+public class hw2 {
+
+    // public void printPrimeNums(){
+    //     boolean isPrime;
+    //     for(int i = 1; i < 1000; i++) {
+    //         isPrime = i == 1;
+    //         for(int j = 2; j < 1000; j++) {
+    //             if (i >= j && i % j == 0) {
+    //                 if(j == i) {
+    //                     isPrime = true;
+    //                 }
+    //                 break;
+    //             }
+    //         }
+    //         if(isPrime) System.out.println(i);
+    //     }
+    // }
+    
+    public static void printPrimeNums() {
+        for (int i = 1; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + "   ");
+            }
+        }
+    }
+    
+    public static boolean isPrime(int n) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        printPrimeNums();
+    }
+}
