4e64baa7ab28dfb13da383d706cc7cef5a92f8fe
Mary <maryqueenk1@gmail.com>
2023-10-31 17:34:03 +0300

    attestation

---
+++ algorithms and data structures/ls2. data structures arrays array algorithms/attestatin.java
@@ -0,0 +1,101 @@
+public class attestatin {
+    public static void main(String[] args) {
+        attestatin tree = new attestatin();
+
+        // Добавляем элементы в дерево
+        tree.insert(10);
+        tree.insert(5);
+        tree.insert(15);
+        tree.insert(3);
+        tree.insert(7);
+        tree.insert(12);
+        tree.insert(18);
+
+        // Выводим элементы дерева в порядке возрастания
+        System.out.println("Элементы дерева в порядке возрастания:");
+        printInOrder(tree.root);
+    }
+
+    private static void printInOrder(attestatin.Node node) {
+        if (node != null) {
+            printInOrder(node.left);
+            System.out.print(node.key + " ");
+            printInOrder(node.right);
+        }
+    }
+
+    private Node root;
+
+    private static class Node {
+        int key;
+        Node left;
+        Node right;
+        boolean isRed;
+
+        Node(int key) {
+            this.key = key;
+            this.isRed = true;
+        }
+    }
+
+    public void insert(int key) {
+        root = insert(root, key);
+        root.isRed = false; // Корень всегда черный
+    }
+
+    private Node insert(Node node, int key) {
+        if (node == null) {
+            return new Node(key);
+        }
+
+        if (key < node.key) {
+            node.left = insert(node.left, key);
+        } else if (key > node.key) {
+            node.right = insert(node.right, key);
+        }
+
+        // Балансировка после вставки
+        if (isRed(node.right) && !isRed(node.left)) {
+            node = rotateLeft(node);
+        }
+        if (isRed(node.left) && isRed(node.left.left)) {
+            node = rotateRight(node);
+        }
+        if (isRed(node.left) && isRed(node.right)) {
+            flipColors(node);
+        }
+
+        return node;
+    }
+
+    private boolean isRed(Node node) {
+        if (node == null) {
+            return false;
+        }
+        return node.isRed;
+    }
+
+    private Node rotateLeft(Node node) {
+        Node newRoot = node.right;
+        node.right = newRoot.left;
+        newRoot.left = node;
+        newRoot.isRed = node.isRed;
+        node.isRed = true;
+        return newRoot;
+    }
+
+    private Node rotateRight(Node node) {
+        Node newRoot = node.left;
+        node.left = newRoot.right;
+        newRoot.right = node;
+        newRoot.isRed = node.isRed;
+        node.isRed = true;
+        return newRoot;
+    }
+
+    private void flipColors(Node node) {
+        node.isRed = !node.isRed;
+        node.left.isRed = !node.left.isRed;
+        node.right.isRed = !node.right.isRed;
+    }
+}
