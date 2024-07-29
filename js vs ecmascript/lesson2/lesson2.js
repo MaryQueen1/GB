c771f7da3d88a69e200da641926fd89ce871dd48
Mary <maryqueenk1@gmail.com>
2024-07-29 05:05:11 +0300

    ex about balance lesson2

--- js vs ecmascript/lesson2/lesson2.js
+++ js vs ecmascript/lesson2/lesson2.js
@@ -95,36 +95,87 @@ function add() {
 console.log(add.call(obj));
 
 // Создайте класс Person, который имеет свойства name и age, а также
-// метод introduce(), который выводит сообщение в консоль с 
+// метод introduce(), который выводит сообщение в консоль с
 // представлением имени и возраста персоны.
 // const person = new Person("John", 25);
 // person.introduce(); // "My name is John and I'm 25 years old."
 
 class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    introduce() {
-        console.log(`My name is ${this.name} and I'm ${this.age} years old.`);
-    }
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  introduce() {
+    console.log(`My name is ${this.name} and I'm ${this.age} years old.`);
+  }
 }
-const user = new Person("John",25);
+const user = new Person("John", 25);
 user.introduce();
 
 // Создайте класс BankAccount, который представляет банковский счет.
 // У него должны быть свойства accountNumber (номер счета) и balance
-// (баланс), а также методы deposit(amount) для пополнения счета, 
-// withdraw(amount) для снятия денег со счета и метод printBalance, 
+// (баланс), а также методы deposit(amount) для пополнения счета,
+// withdraw(amount) для снятия денег со счета и метод printBalance,
 // который отобразит в консоль текущий баланс счета. Класс должен иметь
-// также статическое свойство bankName со значением "GBank", которое 
+// также статическое свойство bankName со значением "GBank", которое
 // содержит название банка.
- 
-// const account1 = new BankAccount("1234567890", 1000);
+
+class BankAccount {
+  static bankName = "GBank"; // то, что есть у всех
+  // свойство для объекта, а не для класса
+  constructor(accountNumber, balance) {
+    this.accountNumber = accountNumber;
+    this.balance = balance;
+  }
+
+  deposit(amount) {
+    // if (typeof amount != "number")
+    if (!Number.isFinite(amount)) {
+      throw new Error("You enter not a number");
+    }
+    this.checkPennis(amount);
+    if (amount <= 0) {
+      throw new Error("Wrong deposit amount, must be a positiv number");
+    }
+    this.balance += amount;
+  }
+
+  withdraw(amount) {
+    if (typeof amount === "number") {
+      this.checkPennis(amount);
+      if (amount > this.balance) {
+        throw new Error(`Insufficient funds in account ${this.accountNumber}`);
+      } else if (amount < 0) {
+        throw new Error("Wrong deposit amount, must be a positiv number");
+      } else {
+        this.balance -= amount;
+      }
+    } else {
+      throw new Error("You enter not a number");
+    }
+  }
+
+  printBalance() {
+    console.log(
+      `${BankAccount.bankName} account ${this.accountNumber} balance: ${this.balance}`
+    );
+  }
+
+  checkPennis(amount) {
+    const arr = amount.toString().split(".");
+    if (arr[1]?.length > 2) {
+      // не будет выполнять последовательность, если значени до оператора было null или underfined
+      throw new Error("Bad boy, wrong format");
+    }
+  }
+}
+
+const account1 = new BankAccount("1234567890", 1000);
+account1.deposit(322.23);
 // account1.deposit(500);
 // account1.withdraw(200);
 // account1.withdraw(1500); // Insufficient funds in account "1234567890"
 // account1.withdraw(-200); // Wrong deposit amount, must be a positive number.
 // account1.deposit(-500); // Wrong deposit amount, must be a positive number.
 // account1.deposit(Infinity); // Wrong deposit amount, must be a positive number.
-// account1.printBalance(); // GBank account "1234567890" balance: 1300
+account1.printBalance(); // GBank account "1234567890" balance: 1300
