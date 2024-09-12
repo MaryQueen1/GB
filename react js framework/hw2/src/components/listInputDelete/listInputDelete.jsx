14b017ddc9e12ba25ca46383cf8c36208e32acdc
Mary <maryqueenk1@gmail.com>
2024-09-12 18:58:21 +0300

    hw1 + todo

--- react js framework/hw2/src/components/listInputDelete/listInputDelete.jsx
+++ react js framework/hw2/src/components/listInputDelete/listInputDelete.jsx
@@ -3,6 +3,50 @@ import { useState } from "react";
 function ListInputDelete() {
   const [text, setText] = useState([]);
   const [input, setInput] = useState("");
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleAddText = () => {
+    if (input.trim()) {
+      setText([...text, { input, id: crypto.randomUUID() }]);
+      setInput("");
+    }
+  };
+
+  const enterClick = (e) => {
+    if (e.key === "Enter") {
+      handleAddText();
+    }
+  };
+
+  const handleDeleteText = (id) => {
+    const updateText = text.filter((thisText) => thisText.id != id);
+    setText(updateText);
+  };
+
+  return (
+    <div>
+      <h2>Какие-то письмена</h2>
+      <input
+        type="text"
+        value={input}
+        onChange={handleInputChange}
+        onKeyDown={enterClick}
+        placeholder="Введите параграф летописи"
+      />
+      <button onClick={handleAddText}>Увековечить</button>
+      <ul>
+        {text.map((text) => (
+          <li key={text.id}>
+            {text.input}
+            <button onClick={() => handleDeleteText(text.id)}>Сжечь</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default ListInputDelete
+export default ListInputDelete;
