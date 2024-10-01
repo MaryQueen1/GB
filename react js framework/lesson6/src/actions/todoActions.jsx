5312669248601a52fc4dc54e350ca9caeb2cf675
Mary <maryqueenk1@gmail.com>
2024-10-01 23:02:10 +0300

    constants and action functions for task management in Redux are implemented реалзуются

---
+++ react js framework/lesson6/src/actions/todoActions.jsx
@@ -0,0 +1,15 @@
+// константы для типов действий
+export const ADD_TODO = "ADD_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+
+// создание задачи
+export const addTodo = (text) => ({
+  type: ADD_TODO, // тип действия
+  payload: { text }, // полезная нагрузка действия
+});
+
+// изменение статуса задачи (выполнено/нет)
+export const toggleTodo = (id) => ({
+  type: TOGGLE_TODO,
+  payload: { id },  
+});
