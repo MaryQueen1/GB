9fa53a281191dfed547bee890226ed0364b8a17e
Mary <maryqueenk1@gmail.com>
2024-10-04 21:01:29 +0300

    data - products

---
+++ react js framework/hw6/src/data/data.jsx
@@ -0,0 +1,41 @@
+const initialProducts = [
+  {
+    id: "1",
+    name: "Samsung Galaxy S23",
+    description:
+      "Флагманский смартфон с отличной камерой и производительностью",
+    price: 20,
+    available: true,
+  },
+  {
+    id: "2",
+    name: "iPhone 15",
+    description:
+      "Последняя модель iPhone с новыми функциями и улучшенной камерой",
+    price: 10.9,
+    available: true,
+  },
+  {
+    id: "3",
+    name: "Google Pixel 8",
+    description: "Смартфон с чистым Android и лучшими камерами",
+    price: 9,
+    available: false,
+  },
+  {
+    id: "4",
+    name: "OnePlus 11",
+    description: "Смартфон с высокой производительностью и быстрой зарядкой",
+    price: 15.1,
+    available: true,
+  },
+  {
+    id: "5",
+    name: "Xiaomi Mi 13",
+    description: "Смартфон с хорошим соотношением цены и качества",
+    price: 11,
+    available: false,
+  },
+];
+
+export default initialProducts;
