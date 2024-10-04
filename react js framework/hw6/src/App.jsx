4efd3cd9831433e2df2f761e2e6c300eb6d9bd10
Mary <maryqueenk1@gmail.com>
2024-10-04 20:56:46 +0300

    hw6 - catalog

--- react js framework/hw6/src/App.jsx
+++ react js framework/hw6/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Provider } from "react-redux";
+import "./App.css";
+import store from "./redux/store";
+import AddProduct from "./components/AddProduct/AddProduct";
+import ProductList from "./components/ProductList/ProductList";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Provider store={store}>
+      <div className="App">
+        <h1>Product Catalog</h1>
+        <AddProduct />
+        <ProductList />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Provider>
+  );
 }
 
-export default App
+export default App;
